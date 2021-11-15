629b6fb59c92c0a9819f563cdfeccc53467fa550
semura823 <muravevartem@outlook.com>
2021-11-15 14:14:35 +0400

    logging by print

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-
 # Константы
+
 E = 0.0001
 X0 = 0.
 Y0 = 1.
@@ -10,18 +9,6 @@ START_N = 4
 # Настройки для записи таблицы
 FILENAME = 'log.csv'
 SPLIT_SYMBOL = ';'
-
-# Логирование
-FILENAME_LOG = None
-LEVEL_LOG = logging.DEBUG
-
-# filename - имя файла с логами, если нужен, то нужно ввести название файла (Информацией о выполнении),
-# туда перенаправляются все выводы с логера: logger.info, logger.debug и т.д.
-# level - выбор того, что будет выводиться:
-# DEBUG - выводит всё,
-# INFO - выводит только выводы логера: logger.info
-logging.basicConfig(level=LEVEL_LOG, filename=FILENAME_LOG)
-logger = logging.getLogger("main.py")
 
 
 def write_to_csv(f, x, y_h, y_h_2, max_d, max_t):
@@ -63,31 +50,30 @@ def get_arr_y_k(n, h):
         x_k = get_x(i - 1, h)
         y_k = y_arr[len(y_arr) - 1]
         y_k_1 = get_y_k_1(x_k, y_k, h)
-        logger.debug(f"Y(k+1): {y_k_1}")
         y_arr.append(y_k_1)
     return y_arr
 
 
 def main():
-    logger.info(f"----------------PARAMS---------------------")
-    logger.info(f"SPLIT SYMBOL : '{SPLIT_SYMBOL}'")
+    print(f"----------------PARAMS---------------------")
+    print(f"SPLIT SYMBOL : '{SPLIT_SYMBOL}'")
 
-    logger.info(f"E : {E}")
+    print(f"E : {E}")
     count = 0
     # Уменьшаю интервал в 2 раза, увеличивая количество точек в 2 раза
     n = START_N
     file_table = open(FILENAME, 'w')
-    logger.info(f"FILE : {file_table.name}")
+    print(f"FILE : {file_table.name}")
     file_table.write(
         f"X{SPLIT_SYMBOL}Y(h){SPLIT_SYMBOL}Y(h/2){SPLIT_SYMBOL}|Y(h) - Y(h/2)|{SPLIT_SYMBOL}|Y(h/2) - Yточ|\n")
     # max|Yn-Yn/2|
     max_d = 0
     # max|Yn/2 - Yточн|
     max_t = 0
-    logger.info(f"-------------------------------------------\n")
-    logger.info(f"------------START CALCULATE----------------")
+    print(f"-------------------------------------------\n")
+    print(f"------------START CALCULATE----------------")
     y_arr_old = get_arr_y_k(n, get_h(n))
-    logger.info(y_arr_old)
+    print(y_arr_old)
     while True:
         count += 1
         n *= 2
@@ -95,16 +81,13 @@ def main():
         h = get_h(n)
         # новый набор значений Y
         y_arr = get_arr_y_k(n, h)
-        logger.info(y_arr)
+        print(y_arr)
         max_d_local = 0
         for i in range(len(y_arr)):
             x_k = get_x(i, h)
             y_k = y_arr[i]
-            logger.debug(f"X: {x_k}")
-            logger.debug(f"Y: {y_k}")
             # Разность с точным решением
             t = abs(y_k - get_y_t(x_k))
-            logger.debug(f"|Yn/2 - Yточн|: {t}")
             if t > max_t:
                 max_t = t
             # Дополнительное поведение для чисел, с одинаковым шагом из предыдущего
@@ -113,7 +96,6 @@ def main():
                 y_k_previous = y_arr_old[i // 2]
                 # Разность между новым и старым
                 d = abs(y_k - y_k_previous)
-                logger.debug(f"|Yn-Yn/2|: {d}")
                 if d > max_d_local:
                     max_d_local = d
                 # Записываем в таблицу
@@ -130,13 +112,13 @@ def main():
         y_arr_old = y_arr
         file_table.write("\n\n")
     # Навёл красоты на вывод)
-    logger.info(f"--------------END CALCULATE-----------------\n")
-    logger.info(f"----------------RESULTS---------------------")
-    logger.info(f"Кол-во уменьшений шага: {count}")
-    logger.info(f"max|Y(n) - Y(n/2)| : {max_d}")
-    logger.info(f"max|Y(n/2) - Yточн| : {max_t}")
-    logger.info(f"Таблицы : {FILENAME}")
-    logger.info(f"--------------------------------------------\n")
+    print(f"--------------END CALCULATE-----------------\n")
+    print(f"----------------RESULTS---------------------")
+    print(f"Кол-во уменьшений шага: {count}")
+    print(f"max|Y(n) - Y(n/2)| : {max_d}")
+    print(f"max|Y(n/2) - Yточн| : {max_t}")
+    print(f"Таблицы : {FILENAME}")
+    print(f"--------------------------------------------\n")
 
 
 main()
